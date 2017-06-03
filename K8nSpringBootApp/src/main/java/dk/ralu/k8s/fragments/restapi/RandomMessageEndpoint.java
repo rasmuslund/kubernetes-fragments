ede165e66a006080fc4e7e6eb6a196f5d115f9e8
Rasmus Lund <rasmus@ralu.dk>
2017-06-03 09:15:06 +0200

    Renamed K8sSpringBootApp groupId and package name

---
+++ K8nSpringBootApp/src/main/java/dk/ralu/k8s/fragments/restapi/RandomMessageEndpoint.java
@@ -0,0 +1,24 @@
+package dk.ralu.k8s.fragments.restapi;
+
+import dk.ralu.k8s.fragments.beans.MessageGenerator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RandomMessageEndpoint {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RandomMessageEndpoint.class);
+
+    @Autowired
+    private MessageGenerator messageGenerator;
+
+    @RequestMapping("/messages/random")
+    public String index() {
+        String generatedMessage = messageGenerator.generateMessage();
+        LOGGER.info("Generated message {}", generatedMessage);
+        return generatedMessage;
+    }
+}
--- K8nSpringBootApp/src/main/java/dk/ralu/springtutorial/profile/TimeProvider.java
+++
@@ -1,8 +0,0 @@
-package dk.ralu.springtutorial.profile;
-
-import java.time.Instant;
-
-public interface TimeProvider {
-
-    Instant now();
-}
